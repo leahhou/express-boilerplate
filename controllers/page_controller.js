c9e1d1297e8693f614323dd547291116957d102a
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-06-18 12:06:15 +1000

    added in register

--- controllers/page_controller.js
+++ controllers/page_controller.js
@@ -3,6 +3,11 @@ function index(req, res) {
     res.json(req.session.views);
 }
 
+function dashboard(req, res) {
+    res.send("Your Dashboard");
+}
+
 module.exports = {
-    index
+    index,
+    dashboard
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const PageController = require("./../controllers/page_controller");
+const AuthenticationController = require("./../controllers/authentication_controller");
+const { Joi, celebrate } = require("celebrate");
 
 router.get("/", PageController.index);
+
+router.get("/register", AuthenticationController.registerNew);
+
+router.post("/register", celebrate({
+    body: {
+        email: Joi.string().required(),
+        password: Joi.string().required()
+    }
+}), AuthenticationController.register);
+
+router.get("/dashboard", PageController.dashboard);
 
 module.exports = router;
